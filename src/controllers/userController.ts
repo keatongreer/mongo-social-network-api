4d73307ff9bd6c1ed4716066a3b835270b271d61
Keaton Greer <keatongreer1@gmail.com>
2024-12-09 18:16:17 -0500

    Added user update route and controller method

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -57,6 +57,31 @@ export const deleteUser = async (req: Request, res: Response) => {
   }
 };
 
+// update user
+export const updateUser = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const { username, email } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { username, email },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred while updating the user.", error });
+  }
+};
+
 // add a friend to a user
 export const addFriend = async (req: Request, res: Response) => {
   const { userId } = req.params;
@@ -79,10 +104,10 @@ export const addFriend = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found." });
     }
 
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (error) {
     console.error("Error adding friend:", error);
-    res
+    return res
       .status(500)
       .json({ message: "An error occurred while adding the friend.", error });
   }
@@ -103,9 +128,9 @@ export const deleteFriend = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found." });
     }
 
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (error) {
     console.error("Error removing friend:", error);
-    res.status(500).json({ message: "An error occurred:", error });
+    return res.status(500).json({ message: "An error occurred:", error });
   }
 };
--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,24 +1,26 @@
 import { Router } from "express";
 const router = Router();
-// import {
-//   getAllStudents,
-//   getStudentById,
-//   createStudent,
-//   deleteStudent,
-//   addAssignment,
-//   removeAssignment,
-// } from "../../controllers/studentController.js";
 
-// /api/students
-// router.route("/").get(getAllStudents).post(createStudent);
+import {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  addFriend,
+  deleteFriend,
+} from "../../controllers/userController";
 
-// /api/students/:studentId
-// router.route("/:studentId").get(getStudentById).delete(deleteStudent);
+// /api/users
+router.route("/").get(getAllUsers).post(createUser);
 
-// /api/students/:studentId/assignments
-// router.route("/:studentId/assignments").post(addAssignment);
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// /api/students/:studentId/assignments/:assignmentId
-// router.route("/:studentId/assignments/:assignmentId").delete(removeAssignment);
+// /api/users/:userId/friends/
+router.post("/:userId/friends", addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.delete("/:userId/friends/:friendId", deleteFriend);
 
 export { router as userRouter };
