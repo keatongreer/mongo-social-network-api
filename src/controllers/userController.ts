ef6d0f9431b6f9f7373b4bd04bce01cb5da8fe0a
Keaton Greer <keatongreer1@gmail.com>
2024-12-08 19:05:20 -0500

    Added user controller and routes

---
+++ src/controllers/userController.ts
@@ -0,0 +1,111 @@
+import { ObjectId } from "mongodb";
+import { User, Thought } from "../models/index.js";
+import { Request, Response } from "express";
+
+// get all users
+export const getAllUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// get a single user
+export const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId }).select("-__v");
+
+    if (!user) {
+      return res.status(404).json({ message: "No user with that ID" });
+    }
+
+    res.json(user);
+    return;
+  } catch (err) {
+    res.status(500).json(err);
+    return;
+  }
+};
+
+// create a new user
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// delete a user
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+    if (!user) {
+      return res.status(404).json({ message: "No user with that ID" });
+    }
+    res.json({ message: "User deleted!" });
+    return;
+  } catch (err) {
+    res.status(500).json(err);
+    return;
+  }
+};
+
+// add a friend to a user
+export const addFriend = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const { friendId } = req.body;
+
+  try {
+    if (userId === friendId) {
+      return res
+        .status(400)
+        .json({ message: "You cannot add yourself as a friend." });
+    }
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $addToSet: { friends: friendId } },
+      { new: true, runValidators: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error adding friend:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while adding the friend.", error });
+  }
+};
+
+// remove a friend
+export const deleteFriend = async (req: Request, res: Response) => {
+  const { userId, friendId } = req.params;
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $pull: { friends: friendId } },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error removing friend:", error);
+    res.status(500).json({ message: "An error occurred:", error });
+  }
+};
